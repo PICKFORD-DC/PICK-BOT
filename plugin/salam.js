ebb8ce5f35c863b3f998d146381c1f0c8fb302f2
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-09-04 04:38:29 +0700

    Update salam.js

--- plugin/salam.js
+++ plugin/salam.js
@@ -1,1 +1,16 @@
 
+let fs = require('fs')
+let handler = async (m) => {
+let helloaine = fs.readFileSync('./MP3/assalamualaikum.opus')
+conn.sendFile(m.chat, helloaine, '', '', m, true)
+}
+handler.help = ['kak desah dong']
+
+handler.tags = ['gabut']
+handler.customPrefix = /^(Assalamualaikum)$/i
+handler.command = new RegExp
+
+handler.limit = true
+
+
+module.exports = handler
